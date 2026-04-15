e6ef7372e5fd535d9f572d1b0b818b7cb7a0a343
DanyloKuch <kucherukdanylo@gmail.com>
2026-04-15 17:20:59 +0300

    fix

--- task1a.hs
+++ task1a.hs
@@ -1,30 +1,22 @@
 import System.IO (hSetEncoding, stdout, utf8)
 
--- 1. Власна реалізація nub (видаляє дублікати)
--- Працює так: беремо перший елемент, а з решти списку видаляємо всі такі ж елементи
 myNub :: Eq a => [a] -> [a]
 myNub [] = []
 myNub (x:xs) = x : myNub (filter (/= x) xs)
 
--- 2. Власна реалізація sort (алгоритм Quicksort)
--- Розбиваємо список на менші за 'x' та більші за 'x', і з'єднуємо їх
 mySort :: Ord a => [a] -> [a]
 mySort [] = []
 mySort (x:xs) = mySort [a | a <- xs, a < x] ++ [x] ++ mySort [a | a <- xs, a >= x]
 
--- 3. Функція для вилучення N найбільших
 removeNLargest :: Int -> [Int] -> [Int]
 removeNLargest n xs
     | n <= 0    = xs
     | null xs   = []
     | otherwise = filter (`notElem` toRemove) xs
   where
-    -- Створюємо список значень для видалення
-    -- reverse, take та filter — це вбудовані функції Prelude, їх імпортувати не треба
     uniqueSorted = reverse (mySort (myNub xs))
     toRemove = take n uniqueSorted
 
--- Перевірка вводу (залишаємо як була)
 isValidInput :: String -> Bool
 isValidInput s = all (\c -> c == ' ' || c == '-' || c `elem` "0123456789") s
 
